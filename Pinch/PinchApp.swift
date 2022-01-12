f752acc67332a60664e13dc4679720ad89891f32
Dalton <daltonulrich2002@gmail.com>
2022-01-11 20:22:34 -0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Dalton Ulrich on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
