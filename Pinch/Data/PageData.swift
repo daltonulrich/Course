24632ab0d971f16406ca6484dc0b0e656bf5ef04
Dalton <daltonulrich2002@gmail.com>
2022-01-12 15:30:45 -0800

    .

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Dalton Ulrich on 1/12/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id:1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
